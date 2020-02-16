44e33ef42108918f7e37e8dfb12d9a96b8857658
woodRock <j.r.h.wood98@gmail.com>
2020-02-17 01:56:53 +1300

    Insensitive Case & Partial Search

--- APP/bug-tracker/src/app/util/search-filter-pipe.ts
+++ APP/bug-tracker/src/app/util/search-filter-pipe.ts
@@ -9,6 +9,8 @@ export class SearchFilterPipe implements PipeTransform {
  transform(items: any[], field: string, value: string): any[] {
    if (value == "") return items;
    if (!items) return [];
-   return items.filter(it => it[field] == value);
+   return items.filter(it =>
+     it[field].toLowerCase().includes(value.toLowerCase())
+   )
  }
 }
